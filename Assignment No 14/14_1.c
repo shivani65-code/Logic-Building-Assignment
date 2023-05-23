2884b49acc9a7585c5a6076f3d497c3d19285473
shivani65-code <129270548+shivani65-code@users.noreply.github.com>
2023-05-23 14:54:25 +0530

    Add files via upload

---
+++ Assignment No 14/14_1.c
@@ -0,0 +1,22 @@
+// Accept number from user and display below pattern.
+// Input : 5
+// Output: A    B    C   D  E
+#include<stdio.h>
+void Pattern(int iNo)
+{
+    int iCnt =0;
+    char ch ='A';
+    for(iCnt=1,ch='A'; iCnt<=iNo;iCnt++,ch++)
+    {
+        printf("%c\t",ch);
+    }
+
+}
+int main()
+{
+    int iValue =0;
+    printf("Enter number of elements\n");
+    scanf("%d",&iValue);
+    Pattern (iValue);
+    return 0;
+}
