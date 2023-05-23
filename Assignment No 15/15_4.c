17603bddd0b20fd63877123a97c289060855daa1
shivani65-code <129270548+shivani65-code@users.noreply.github.com>
2023-05-23 14:55:10 +0530

    Add files via upload

---
+++ Assignment No 15/15_4.c
@@ -0,0 +1,36 @@
+//Accept number from rows and number of columns from user and display below pattern
+// Input : iRow =3   iCol =4
+// output : * # * #
+//          * # * #
+//          * # * #
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+int i=0,j=0;
+for(i=1;i<=iRow;i++)
+{
+    for(j=1;j<=iCol;j++)
+    {
+        if((i % 2)==0)
+        {
+            printf("#\t");
+        }
+        else
+        {
+            printf("*\t");
+        }
+    }
+    printf("\n");
+    
+}
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+   printf("Enter number of rows:\n");
+   scanf("%d",&iValue1);
+   printf("Enter number of columns:\n");
+   scanf("%d",&iValue2);
+   Pattern(iValue1,iValue2);
+}
---
+++ Assignment No 15/15_5.c
@@ -0,0 +1,29 @@
+//Accept number of rows and number of columns from user and display below pattern
+// iRow =3   iCol =4
+//output 1  1   1   1
+//       2  2   2   2
+//       3  3   3   3
+//       4  4   4   4
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+int i=0,j=0;
+for(i=1;i<=iRow;i++)
+{
+    for(j=1;j<=iCol;j++)
+    {
+        printf("%d\t",i);
+    }
+    printf("\n");
+}
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+   printf("Enter number of rows:\n");
+   scanf("%d",&iValue1);
+   printf("Enter number of columns:\n");
+   scanf("%d",&iValue2);
+   Pattern(iValue1,iValue2);
+}
