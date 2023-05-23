513dcb73653eee73ad260f0371c8e2fabaf84328
shivani65-code <129270548+shivani65-code@users.noreply.github.com>
2023-05-23 14:57:58 +0530

    Add files via upload

---
+++ Assignment No 19/19_5.c
@@ -0,0 +1,37 @@
+// Accept number of rows and number of columns from user and display below pattern
+//input: iRow =6 iCol =6
+// output :
+
+#include<stdio.h>
+void Pattern(int iRow, int iCol)
+{
+int i=0,j=0;
+
+for(i=1;i<=iRow;i++)
+{
+    for(j=1;j<=iCol;j++)
+    {
+    if((i==1)|| (i==iRow)|| (j==1)||(j==iCol)||(i==j))
+    {
+        printf("*\t");
+    }
+    else
+    {
+        printf(" \t");
+    }
+   
+
+    }
+    printf("\n");
+}
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+
+   printf("Enter number of rows:\n");
+   scanf("%d",&iValue1);
+   printf("Enter number of columns:\n");
+   scanf("%d",&iValue2);
+   Pattern(iValue1,iValue2);
+}
